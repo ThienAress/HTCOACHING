3e13590ed83468cc4b4961ef26c174e9fad873ec
Your Name01~ <you@example.com>
2025-05-11 14:02:25 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Smooth Scroll for Navigation
+// const btn = document.getElementById("scrollToTopBtn");
+
+// window.addEventListener("scroll", () => {
+//   const scrollTop =
+//     document.documentElement.scrollTop || document.body.scrollTop;
+//   const windowHeight = window.innerHeight;
+//   const scrollHeight = document.documentElement.scrollHeight;
+//   const distanceToBottom = scrollHeight - (scrollTop + windowHeight);
+
+//   if (distanceToBottom < 300) {
+//     btn.classList.add("show");
+//   } else {
+//     btn.classList.remove("show");
+//   }
+// });
+
+// btn.addEventListener("click", () => {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// });
